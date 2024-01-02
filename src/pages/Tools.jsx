158525d9185db8e2007ddc6fc23abe3c0b4f751f
chrispine owuor <owuorchrispine77@gmail.com>
2024-01-02 17:43:55 +0300

    resolved imports

--- src/pages/Tools.jsx
+++ src/pages/Tools.jsx
@@ -1,10 +1,10 @@
-import Tech from "../components/tech";
 import "../assets/main.css";
+import Tech from "../components/Tech";
 const Tools = () => {
   return (
     <div className=" tools w-full  flex justify-center ">
       <div className="w-full bg-hero mx-auto bg-fixed">
-        <Tech />
+        <Tech/>
       </div>
     </div>
   );
